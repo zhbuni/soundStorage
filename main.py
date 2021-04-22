25e72f1b4bfb14e7aa5b8e924465dc19ab402ddf
zhbuni <xdff211@gmail.com>
2021-04-22 14:11:36 +0500

    changed requirements.txt

--- main.py
+++ main.py
@@ -380,6 +380,6 @@ def search():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    # port = int(os.environ.get("PORT", 4444))
     # app.run(host='0.0.0.0', port=port)
     app.run()
